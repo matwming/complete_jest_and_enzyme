c7623ad6d33737e68e8a3b07ca37284ff5ab909c
MING WU <matwming114@gmail.com>
2019-04-15 15:45:00 +1000

    tested success actions

--- src/store/successReducer.js
+++ src/store/successReducer.js
@@ -1,7 +1,11 @@
+import { actionTypes } from "../actions/index";
+
 let initialState = {};
 
-export const successReducer = (state = initialState, action) => {
+export const successReducer = (state = false, action) => {
  switch (action.type) {
+  case actionTypes.CORRECT_GUESS:
+   return true;
   default:
    return state;
  }
