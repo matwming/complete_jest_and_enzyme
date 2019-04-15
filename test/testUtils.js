6e322751789cb713a87b7a40de38ae62b17a9602
MING WU <matwming114@gmail.com>
2019-04-15 16:36:15 +1000

    set up input component

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,14 @@
 import checkPropTypes from "check-prop-types";
-
+import { createStore } from "redux";
+import { rootReducer } from "../src/store/index";
+/**
+|--------------------------------------------------
+| 
+|--------------------------------------------------
+*/
+export const storeFactory = initialState => {
+ return createStore(rootReducer, initialState);
+};
 /**
 |--------------------------------------------------
 | 
