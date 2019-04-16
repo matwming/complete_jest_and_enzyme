855aaecc3e1ba26c54d89d314d1a6e659b81a877
MING WU <matwming114@gmail.com>
2019-04-16 10:45:37 +1000

    added getLetterMatchCount function

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+/**
+|--------------------------------------------------
+| 
+|--------------------------------------------------
+*/
+export function getLetterMatchCount(guessedWord, secretWord) {
+ const secretLetterSet = new Set(secretWord.split(""));
+ const guessedLetterSet = new Set(guessedWord.split(""));
+ return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
+}
